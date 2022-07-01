f71d3702b0de36d86aa8405cdce54c88962c828f
tanveer <mdtanviralamht731@gmail.com>
2022-07-01 08:14:14 +0600

    check api added

--- index.js
+++ index.js
@@ -2,18 +2,11 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dq2tc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3ruk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -36,6 +29,19 @@ async function run() {
       const addTask = req.body;
       const task = await taskCollection.insertOne(addTask);
       res.send(task);
+    });
+    // checked task  api
+    app.patch("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const taskBody = req.body;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          check: taskBody,
+        },
+      };
+      const result = await taskCollection.updateOne(filter, updatedDoc);
+      res.send(result);
     });
   } finally {
   }
